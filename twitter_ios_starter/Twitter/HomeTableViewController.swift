de110afa0448a77d6913d113d3a8333227222168
shivpvtel <81172033+shivpvtel@users.noreply.github.com>
2022-03-12 00:08:05 -0500

    Unit 3 twitter application

---
+++ twitter_ios_starter/Twitter/HomeTableViewController.swift
@@ -0,0 +1,186 @@
+//
+//  HomeTableViewController.swift
+//  Twitter
+//
+//  Created by Shiv Patel on 3/11/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    // variables that can change
+    var tweetArray = [NSDictionary]()
+    var numberOfTweets: Int!
+    var myRefreshControl = UIRefreshControl()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //calling function that loads the tweet into our user interface
+        loadTweets()
+        //making a pull down refresh options for users
+        myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    //CAllS THE API
+    @objc func loadTweets(){
+        //parameters set
+        numberOfTweets = 20
+        //API URL that has all over info
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        let myParams = ["count": numberOfTweets]
+        //calls information from the website with certain parameters
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: {
+                (tweets: [NSDictionary]) in
+            self.tweetArray.removeAll()
+                    for tweet in tweets {
+                self.tweetArray.append(tweet)
+            }
+            //reloads data
+            self.tableView.reloadData()
+            self.myRefreshControl.endRefreshing()
+            //failure counter measure
+        }, failure: { (Error) in
+            print("could not retrive tweets pt2!")
+        })
+    }
+    
+    
+    
+    // loading more tweets
+    func loadMoreTweets(){
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        numberOfTweets = numberOfTweets + 20
+        let myParams = ["count": numberOfTweets]
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: {
+                (tweets: [NSDictionary]) in
+            self.tweetArray.removeAll()
+                    for tweet in tweets {
+                self.tweetArray.append(tweet)
+            }
+            //reloads data
+            self.tableView.reloadData()
+            //failure counter measure
+        }, failure: { (Error) in
+            print("could not retrive tweets pt2!")
+        })
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath){
+        if indexPath.row + 1 == tweetArray.count{
+            loadMoreTweets()
+        }
+    }
+
+    
+    //Log out function
+    @IBAction func onLogout(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+        UserDefaults.standard.set(false, forKey: "UserLoggedIn")
+    }
+    
+    
+    
+    
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //getting the tweet body information
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCell
+        // getting the username information
+        let user = tweetArray[indexPath.row]["user"] as! NSDictionary
+        //the callers for the twitter name and Tweet body from the API
+        cell.userNameLabel.text = user["name"] as? String
+        cell.tweetContent.text = tweetArray[indexPath.row]["text"] as? String
+        //the callers for getting the image
+        let imageUrl = URL(string: (user["profile_image_url_https"] as? String)!)
+        let data = try? Data(contentsOf: imageUrl!)
+        
+        if let imageData = data {
+            cell.profileImageView.image = UIImage(data: imageData)
+        }
+        return cell
+    }
+    
+    
+    
+    
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows per section
+        return tweetArray.count
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
